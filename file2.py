3b27b8d5d22a9ee8e45d34d57cbd9f9727b58b1c
Ann <akgarbowski@gmail.com>
2024-08-14 20:33:22 -0400

    fixed #1

--- file2.py
+++ file2.py
@@ -7,7 +7,7 @@ from file1 import addNumbers
 
 
 if __name__== "__main__":
-    x = input("Please input a value for x: ")
-    y = input("Please input a value for y: ")
+    x = int(input("Please input a value for x: "))
+    y = int(input("Please input a value for y: "))
 
     print(addNumbers(x,y))
