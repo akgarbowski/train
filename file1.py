df3d56444f933dc052b8c58498badf6faa52f691
Ann <akgarbowski@gmail.com>
2024-08-14 20:27:20 -0400

    Refactoring

---
+++ file1.py
@@ -0,0 +1,12 @@
+# x = 1
+# z = x + y
+# print(z)
+
+# print(x)
+
+
+def addNumbers(x:int, y:int):
+    return x + y
+
+ans = addNumbers(5,7)
+print(ans)
---
+++ file2.py
@@ -0,0 +1,13 @@
+from file1 import addNumbers
+
+# x = input("Please input a value for x: ")
+# y = input("Please input a value for y: ")
+
+# print(addNumbers(x,y))
+
+
+if __name__== "__main__":
+    x = input("Please input a value for x: ")
+    y = input("Please input a value for y: ")
+
+    print(addNumbers(x,y))
