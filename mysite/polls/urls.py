2d141bf5e6ccd4c88e2cac71f0c7d67c123dcb6b
ann <akgarbowski@gmail.com>
2024-08-17 01:08:40 -0400

    added in new paths

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("ann", views.ann, name="ann"),
+    path("calvin/<str:message>", views.calvin, name="calvin"),
+    path("add/<int:num1>/<int:num2>", views.calvin, name="calvin"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
+def ann(request):
     return HttpResponse("Hello, world. You're at polls index.")
+
+def calvin(request, num1:int, num2:int):
+    return HttpResponse(f"Hello, your sum is! \n {num1+num2}")
 
